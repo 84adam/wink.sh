07870a7cd843d9ee0fb9800ee356b6baed1bf9f9
Adam Anderson <84adam@users.noreply.github.com>
2016-04-04 00:16:28 -0700

    remove log files after program terminates

--- wink.sh
+++ wink.sh
@@ -5,16 +5,18 @@
 
 # get current BTC price in cents, log to wink.log
 
-curl -s -H "User-Agent: wink-sh" "https://winkdex.com/api/v0/price" > wink.log
+curl -s -H "User-Agent: wink-sh" "https://winkdex.com/api/v0/price" > $HOME/bin/wink.log
 
 # cut and print BTC value in 'cents', log to cents.log
 
-cat wink.log | cut -d ":" -f 5 | cut -d "," -f 1 > cents.log
+cat $HOME/bin/wink.log | cut -d ":" -f 5 | cut -d "," -f 1 > $HOME/bin/cents.log
 
 # set variable PRICE and calculate dollars and cents using 'bc'
 
-PRICE=$(echo "`cat cents.log` * .01" | bc)
+PRICE=$(echo "`cat $HOME/bin/cents.log` * .01" | bc)
 
 # print PRICE along with current time and attribution
 
 echo "The price for one bitcoin as of `date +\"%r\"` is \$$PRICE. [WINKDEX.COM]"
+
+rm $HOME/bin/wink.log && rm $HOME/bin/cents.log
